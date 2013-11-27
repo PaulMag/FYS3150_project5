97728d5b1010f183a0ed09209dd0ac751d807bbe
PaulMag <paulms@ulrik.uio.no>
2013-11-27 17:00:04 +0100

    Now also have the option to automatically save a movie.

--- run.py
+++ run.py
@@ -8,13 +8,17 @@ if len(argv) >= 4: # solve and write data
     system("mkdir data/%s" % argv[3])
     system("./main.x %s %s %s" % (argv[1], argv[2], argv[3]))
 
-    flag = True
+    flag = 1 # default
     if len(argv) >= 5:
         if argv[4] == "no":
-            flag = False
+            flag = 0
+        elif argv[4] == "film" or argv[4] == "movie":
+            flag = 2
 
-    if flag == True: # also automatically plot results
+    if flag == 1: # automatically plot results
         system("python plot.py %s &" % argv[3])
+    if flag == 2: # automatically save a movie
+        system("python plot.py %s movie &" % argv[3])
 
 else: # solve without writing data
     system("./main.x %s %s 0" % (argv[1], argv[2]))
