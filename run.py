c9b0a348024f92b8d57da7ef584b78d35d1029ff
PaulMag <paulms@ulrik.uio.no>
2013-11-28 16:26:16 +0100

    Shifted argv+=1 since 1 more argument was added.

--- run.py
+++ run.py
@@ -2,27 +2,29 @@ from sys import argv
 import os
 
 # Arguments:
-# time, dt, name(optional, determines if filewrite) (optional plotting args)
+# N, time, dt, name(optional, determines if filewrite) (optional plotting args)
 
 # Solve WITHOUT writing data:
-if len(argv) <= 3:
-    os.system("./main.x %s %s 0" % (argv[1], argv[2])) # run simulation
+if len(argv) <= 4:
+    # Run simulation:
+    os.system("./main.x %s %s %s 0" % (argv[1], argv[2], argv[3]))
 
 # Solve and write data:
 else:
     # Check if path present, else make path:
     if not os.path.exists("data"):
         os.mkdir("data")
-    if not os.path.exists("data/%s" % argv[3]):
-        os.mkdir("data/%s" % argv[3])
-        
-    os.system("./main.x %s %s %s" % (argv[1], argv[2], argv[3]))# run simulation
+    if not os.path.exists("data/%s" % argv[4]):
+        os.mkdir("data/%s" % argv[4])
+
+    # Run simulation:
+    os.system("./main.x %s %s %s %s" % (argv[1], argv[2], argv[3], argv[4]))
 
     # Optional plotting:
-    if len(argv) >= 5:
-        s = "python plot.py %s" % argv[3]
+    if len(argv) >= 6:
+        s = "python plot.py %s" % argv[4]
         
-        for i in range(4, len(argv)): # argv[4] and up is plotting commands
+        for i in range(5, len(argv)): # argv[5] and up is plotting commands
             s += " %s" % argv[i]
 
     os.system(s) # run plot.py
