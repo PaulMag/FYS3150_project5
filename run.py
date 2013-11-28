04e48de12e5c08b0e39c9e5ce0f230bcfa8fce1e
jaremikb <jaremikb@ulrik.uio.no>
2013-11-28 11:10:23 +0100

    Fixed import and path-checking.

--- run.py
+++ run.py
@@ -1,28 +1,30 @@
 from sys import argv
-from os import system,mkdir
+import os
 
 # Usage:
 # time, dt, name (determine if filewrite), "no" (optional, turns off plotting)
 
-if len(argv) >= 4: # solve and write data
-    # Check if path present
-    if not os.path.exists("data/%s" % argv[3]):
-        # If not, create dir
-        mkdir("data/%s" % argv[3])
-        
-    system("./main.x %s %s %s" % (argv[1], argv[2], argv[3]))
+if __name__ == '__main__':
+    if len(argv) >= 4: # solve and write data
+        # Check if path present
+        if not os.path.exists("data"):
+            os.mkdir("data")
+        if not os.path.exists("data/%s" % argv[3]):
+            os.mkdir("data/%s" % argv[3])
+            
+        os.system("./main.x %s %s %s" % (argv[1], argv[2], argv[3]))
 
-    flag = 1 # default
-    if len(argv) >= 5:
-        if argv[4] == "no":
-            flag = 0
-        elif argv[4] == "film" or argv[4] == "movie":
-            flag = 2
+        flag = 1 # default
+        if len(argv) >= 5:
+            if argv[4] == "no":
+                flag = 0
+            elif argv[4] == "film" or argv[4] == "movie":
+                flag = 2
 
-    if flag == 1: # automatically plot results
-        system("python plot.py %s &" % argv[3])
-    if flag == 2: # automatically save a movie
-        system("python plot.py %s movie &" % argv[3])
+        if flag == 1: # automatically plot results
+            os.system("python plot.py %s &" % argv[3])
+        if flag == 2: # automatically save a movie
+            os.system("python plot.py %s movie &" % argv[3])
 
-else: # solve without writing data
-    system("./main.x %s %s 0" % (argv[1], argv[2]))
+    else: # solve without writing data
+        os.system("./main.x %s %s 0" % (argv[1], argv[2]))
